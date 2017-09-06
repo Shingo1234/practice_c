c05b714c6cadc1477666a4679a10025be1a423ae
KinoshitaShingo <kinoshita.shingo179@mail.kyutech.jp>
2017-09-06 13:55:04 +0900

    First commit
    練習用

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+  int a; 
+  printf("グー、チョキ、パーを入力してください。:");
+  printf("\n");
+  scanf("%d",&a);
+  while{
+    if (ー)
+    printf("パー");
+  }
+ return 0;c
+}
+
---
+++ 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int i;
+  i=rand()*2;
+  printf{グー、チョキ、パーを入力してください}
+  if(グー)//Wont work
+    {printf a
+        };
+  if(チョキ)
+    {printf i
+        }
+  
+    
+  return(0);
+}
